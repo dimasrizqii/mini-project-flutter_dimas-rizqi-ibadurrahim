cac72672af6c9ace13fdfefdb847b7656d79f4b6
Dimas Rizqi Ibadurrahim <akbarabi72@gmail.com>
2023-04-18 23:26:11 +0700

    Initial commit

---
+++ lib/constant/tmdb_api_constant.dart
@@ -0,0 +1,4 @@
+const String baseUrl = 'https://api.themoviedb.org/3';
+const String imageW500Url = 'https://image.tmdb.org/t/p/w500';
+const String imageOriginalUrl = 'https://image.tmdb.org/t/p/original';
+const String apiKey = 'a97da0107371a8cbbff5148d88b92840';
